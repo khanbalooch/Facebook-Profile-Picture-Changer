086f97c45d165a2cb1aaf5916516322e0b8916c7
tayyab4web <dev.tayyab@gmail.com>
2018-05-07 21:58:15 +0500

    fb post frontend

--- src/app/components/contentarea/show-profile/show-profile.component.ts
+++ src/app/components/contentarea/show-profile/show-profile.component.ts
@@ -8,13 +8,18 @@ import { User } from '../User';
   styleUrls: ['./show-profile.component.css']
 })
 export class ShowProfileComponent implements OnInit {
-
+  public backgroundImg: any;
   user: User;
+  
 
   constructor(private tokenService: TokenService) {
+    
     this.tokenService.currentToken.subscribe(token => this.user =  User.parse( token));
   }
+
   ngOnInit() {
+    this.backgroundImg = this.user.profilePicture;
   }
+  
 
 }
